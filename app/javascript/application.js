0452ffc05d502752ffa45fc9c28c7354baed6b5f
Apple <apple@appurunoMacBook-Pro.local>
2024-10-23 21:30:37 +0900

    application.jsを統合し、重複を削除

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import { Application } from "@hotwired/stimulus"
+
+// Stimulusの設定
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus = application
+
+export { application }
