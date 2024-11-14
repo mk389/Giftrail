bc59f83447a9743e6d2d65de93c78206011c322e
Apple <apple@appurunoMacBook-Pro.local>
2024-11-14 22:36:12 +0900

    Postモデルとビューの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,12 +14,21 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id
 
     if @post.save
+      # 画像がアップロードされた場合
+      if params[:post][:image].present?
+        @post.image.attach(params[:post][:image])
+      end
+
       flash[:notice] = '投稿が作成されました！'
-      redirect_to @post
+      redirect_to @post  # 投稿が作成された後にその投稿詳細ページにリダイレクト
     else
       flash.now[:alert] = '投稿に失敗しました。'
       render :new
     end
+  end
+
+  def show
+    @post = Post.find(params[:id])  # params[:id] で指定された投稿を取得
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-# モデルの関連付け
   belongs_to :user  # userとの関連を設定
+  has_one_attached :image  # 画像を添付する設定
 
 # バリデーション（必要に応じて追加）
   validates :title, presence: true
