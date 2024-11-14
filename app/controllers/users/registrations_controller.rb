9be74027a1ba1e9ba80de5fade67d044b64e715e
Apple <apple@appurunoMacBook-Pro.local>
2024-11-14 22:42:14 +0900

    Deviseのセッション画面、フラッシュメッセージ、Tailwind設定の変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,20 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def create
     super do |resource|
-      if resource.persisted?
-        flash[:notice] = '登録が完了しました'
-        redirect_to root_path and return # 正常な場合、リダイレクト
-      else
-        flash[:alert] = '登録に失敗しました'
-        # フォームに戻す際、リダイレクトを追加してみてください
-        redirect_to new_user_registration_path and return # 失敗時でもリダイレクト
+      if resource.errors.any? # もしエラーがあれば
+        flash[:alert] = I18n.t("devise.registrations.failure") # カスタムメッセージを設定
       end
     end
   end
 end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
