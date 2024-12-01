60c08369f05fe99bd315d812c08657e53d93fe2f
Apple <apple@appurunoMacBook-Pro.local>
2024-12-01 21:46:31 +0900

    レイアウト修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,17 +6,6 @@ import "./controllers"
 // Stimulusの設定
 const application = Application.start()
 
-const swiper = new Swiper('.swiper', {
-  loop: true, // スライダーをループさせる
-  pagination: {
-    el: '.swiper-pagination', // ページネーションを有効にする
-  },
-  navigation: {
-    nextEl: '.swiper-button-next', // 次へボタン
-    prevEl: '.swiper-button-prev', // 前へボタン
-  },
-});
-
 application.debug = false
 window.Stimulus = application
 
