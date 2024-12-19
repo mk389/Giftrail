2751b7f21341ea4273b5801f18e21946da1b8761
Apple <apple@appurunoMacBook-Pro.local>
2024-12-19 23:18:13 +0900

    google認証

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,8 +1,5 @@
 Devise.setup do |config|
-  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
-  scope: 'email,profile',
-  redirect_uri: 'https://giftrail.net/users/auth/google_oauth2/callback'
-  }
+  config.omniauth :google_oauth2,ENV['GOOGLE_CLIENT_ID'],ENV['GOOGLE_CLIENT_SECRET'],skip_jwt: true
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
   
   require 'devise/orm/active_record'
