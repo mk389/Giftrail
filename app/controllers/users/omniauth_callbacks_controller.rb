a4ed9f179029d0ad4037185bc16bb96c10e3a1ff
Apple <apple@appurunoMacBook-Pro.local>
2024-12-20 00:15:24 +0900

    google認証

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def callback_for(provider)
     @user = User.from_omniauth(request.env['omniauth.auth'])
-
+    @user.skip_confirmation!
+    @user.save!
+    
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: provider.to_s.capitalize) if is_navigational_format?
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,5 @@
 Devise.setup do |config|
-  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'],ENV['GOOGLE_CLIENT_SECRET'],
                   scope: 'email,profile',
                   redirect_uri: "#{ENV['HOST']}/users/auth/google_oauth2/callback"
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
