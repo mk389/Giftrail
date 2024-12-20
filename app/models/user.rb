4e4f2538250f5e9ce6b5c304897527a618b9609d
Apple <apple@appurunoMacBook-Pro.local>
2024-12-20 23:58:06 +0900

    google認証

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
+         :omniauthable, omniauth_providers: [:google_oauth2],
+         :confirmable
 
   has_many :posts, dependent: :destroy
 
@@ -11,28 +10,35 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :residence, presence: true
-  validates :uid, presence: true, uniqueness: { scope: :provider }, if: -> { uid.present? }
+  validates :uid, presence: true, uniqueness: { scope: :provider }
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.name = auth.info.name
-      user.email = auth.info.email
-      user.icon = auth.info.image
-      user.password = Devise.friendly_token[0, 20]
-      user.residence ||= '不明'
+    user = find_by(provider: auth.provider, uid: auth.uid)
 
-      if user.save
+    if user
+      user
+    else
+      user = find_by(email: auth.info.email)
+      if user
+        user.update(provider: auth.provider, uid: auth.uid)
         user
       else
-        user.errors.full_messages.each do |message|
-          Rails.logger.error message  # ログにもエラーメッセージを出力
+        base_username = auth.info.name.parameterize
+        unique_username = base_username
+        counter = 1
+        while User.exists?(username: unique_username)
+          unique_username = "#{base_username}#{counter}"
+          counter += 1
         end
-        raise StandardError, "User could not be saved: #{user.errors.full_messages.join(", ")}"
+
+        create(
+          email: auth.info.email,
+          username: unique_username,
+          password: Devise.friendly_token[0, 20],
+          provider: auth.provider,
+          uid: auth.uid,
+        )
       end
     end
-  end
-
-  def self.create_unique_string
-    SecureRandom.uuid
   end
 end
