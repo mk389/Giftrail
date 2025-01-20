ff7abc9ddff750939748183006e051b3f83fb533
Apple <apple@appurunoMacBook-Pro.local>
2025-01-20 22:03:35 +0900

    パスワードリセット

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: "yourdomain.com" }
+  config.action_mailer.default_url_options = { host: 'giftrail.net', protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              ENV.fetch('MAIL_ADDRESS'),
